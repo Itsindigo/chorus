421ade03fc3d332d9d4ac3dc7e5be4747e26dfd1
Matthew Bridges <mattbridgesbusiness@gmail.com>
2018-04-23 19:56:10 +0100

    saving model instance to db

--- routes/templateTasks.js
+++ routes/templateTasks.js
@@ -4,15 +4,16 @@ const router = express.Router();
 const models = require('../models');
 
 router.get('/', function(req, res, next) {
-  let instance = models.TemplateTask.build({
-    name: 'first list',
-  })
-  console.log(instance);
-  res.render('index', { title: 'Express' });
+  models.TemplateTask.findAll()
+    .then(function(tasks) {
+      res.render('index', { title: 'Express', tasks: tasks });
+    })
 });
 
 router.post('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let instance = models.TemplateTask.create({
+    name: req.body.task.name,
+  })
 });
 
 module.exports = router;
